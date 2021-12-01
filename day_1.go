7a614baca5d6b68cf18eec121718436b44ca3945
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-01 09:37:55 -0500

    It works for the sample input

--- day_1.go
+++ day_1.go
@@ -4,5 +4,17 @@ import "fmt"
 
 func main() {
 	testInput := []int{199, 200, 208, 210, 200, 207, 240, 269, 260, 263}
-	fmt.Printf("%v\n", testInput)
+	fmt.Printf("Increases: %d\n", countStepIncreases(testInput))
+}
+
+func countStepIncreases(measurements []int) int {
+	previousMeasurement := measurements[0]
+	increases := 0
+	for _, measurement := range measurements[1:] {
+		if previousMeasurement < measurement {
+			increases += 1
+		}
+		previousMeasurement = measurement
+	}
+	return increases
 }
