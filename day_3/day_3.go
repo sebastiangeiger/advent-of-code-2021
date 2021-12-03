fc6357e1d8e548bc82f6e4f4a517930aea2fa6da
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-03 09:54:14 -0500

    Convert the input the a 2d int array

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -2,6 +2,7 @@ package day_3
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -18,9 +19,40 @@ func Run(problem int) {
 }
 
 func problem1() {
-	fmt.Printf("Implement me!\n")
+	solveProblem1("day_3_test.input")
 }
 
 func problem2() {
 	fmt.Printf("Implement me!\n")
+}
+
+func solveProblem1(path string) {
+	lines := common.ReadLinesFrom(path)
+	pivoted := toIntArrays(lines)
+	fmt.Printf("%#v\n", pivoted)
+}
+
+func toIntArrays(lines []string) [][]int {
+	dx := len(lines)
+	dy := len(lines[0])
+	result := initializeArray(dx, dy)
+	for x := 0; x < dx; x++ {
+		for y := 0; y < dy; y++ {
+			number, err := strconv.Atoi(string(lines[x][y]))
+			if err == nil {
+				result[x][y] = number
+			} else {
+				panic(err)
+			}
+		}
+	}
+	return result
+}
+
+func initializeArray(dx int, dy int) [][]int {
+	result := make([][]int, dx)
+	for i := 0; i < dx; i++ {
+		result[i] = make([]int, dy)
+	}
+	return result
 }
