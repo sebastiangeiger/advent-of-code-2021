7de5722a6d3a1735587fc0bb57ae2acca5c17752
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-03 10:19:52 -0500

    Getting the gamma

--- day_3/day_3.go
+++ day_3/day_3.go
@@ -2,6 +2,7 @@ package day_3
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
@@ -29,9 +30,38 @@ func problem2() {
 func solveProblem1(path string) {
 	lines := common.ReadLinesFrom(path)
 	pivoted := pivot(toIntMatrix(lines))
-	fmt.Printf("%#v\n", pivoted)
+	gamma := calculateGamma(pivoted)
+	fmt.Printf("pivoted: %#v\n", pivoted)
+	fmt.Printf("gamma: %#v\n", makeBase10(gamma))
 }
 
+func calculateGamma(input [][]int) []int {
+	output := make([]int, len(input))
+	for i, numbers := range input {
+		sum := 0
+		for _, number := range numbers {
+			sum += number
+		}
+		majority := 0
+		if sum > len(numbers)/2.0 {
+			majority = 1
+		}
+		output[i] = majority
+	}
+	return output
+}
+
+func makeBase10(input []int) int {
+	length := len(input)
+	output := 0
+	for i, number := range input {
+		significance := float64((length - 1) - i)
+		output += int(math.Pow(2, significance) * float64(number))
+	}
+	return output
+}
+
+// This enforces that we get an array of arrays with the same dimensions and the values being 0 or 1
 func toIntMatrix(lines []string) [][]int {
 	dx := len(lines)
 	dy := len(lines[0])
@@ -43,10 +73,12 @@ func toIntMatrix(lines []string) [][]int {
 		}
 		for y := 0; y < dy; y++ {
 			number, err := strconv.Atoi(string(currentLine[y]))
-			if err == nil {
-				result[x][y] = number
-			} else {
+			if err != nil {
 				panic(err)
+			} else if number != 0 && number != 1 {
+				panic(fmt.Sprintf("Got a number that's not 0 or 1: %d", number))
+			} else {
+				result[x][y] = number
 			}
 		}
 	}
