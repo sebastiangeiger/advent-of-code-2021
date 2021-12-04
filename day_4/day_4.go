481a9139bf4e09d0f958a39e58a1eed2930fe383
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-04 15:26:19 -0500

    Parsing drawnNumbers works

--- day_4/day_4.go
+++ day_4/day_4.go
@@ -2,6 +2,8 @@ package day_4
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -28,7 +30,9 @@ func problem2() {
 func solveProblem1(path string) {
 	lines := common.ReadLinesFrom(path, true)
 	partitions := makePartitions(lines)
+	drawnNumbers := makeDrawnNumbers(partitions[0][0])
 	fmt.Printf("partitions: %#v", partitions)
+	fmt.Printf("drawnNumbers: %#v", drawnNumbers)
 }
 
 func makePartitions(lines []string) [][]string {
@@ -44,6 +48,20 @@ func makePartitions(lines []string) [][]string {
 	}
 	if len(currentPartition) > 0 {
 		output = append(output, currentPartition)
+	}
+	return output
+}
+
+func makeDrawnNumbers(input string) []int {
+	split := strings.Split(input, ",")
+	output := make([]int, len(split))
+	for i, s := range split {
+		parsed, err := strconv.Atoi(s)
+		if err == nil {
+			output[i] = parsed
+		} else {
+			panic("Couldn't parse number")
+		}
 	}
 	return output
 }
