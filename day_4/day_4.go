d53c0be280892ff36e2e82dc68a3f0305a7a6d51
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-04 14:51:01 -0500

    Making partitions

--- day_4/day_4.go
+++ day_4/day_4.go
@@ -18,9 +18,32 @@ func Run(problem int) {
 }
 
 func problem1() {
-	fmt.Println("Implement Day 4 - Problem 1")
+	solveProblem1("day_4_test.input")
 }
 
 func problem2() {
 	fmt.Println("Implement Day 4 - Problem 2")
+}
+
+func solveProblem1(path string) {
+	lines := common.ReadLinesFrom(path, true)
+	partitions := makePartitions(lines)
+	fmt.Printf("partitions: %#v", partitions)
+}
+
+func makePartitions(lines []string) [][]string {
+	output := [][]string{}
+	currentPartition := []string{}
+	for _, line := range lines {
+		if len(line) == 0 {
+			output = append(output, currentPartition)
+			currentPartition = []string{}
+		} else {
+			currentPartition = append(currentPartition, line)
+		}
+	}
+	if len(currentPartition) > 0 {
+		output = append(output, currentPartition)
+	}
+	return output
 }
