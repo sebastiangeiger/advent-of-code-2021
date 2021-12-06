f8005a94ddf08cea5fc87c6042fcbd863885a833
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-06 02:29:05 +0000

    Points work

--- common/common.go
+++ common/common.go
@@ -57,3 +57,13 @@ func Max(numbers ...int) int {
 	}
 	return max
 }
+
+func Min(numbers ...int) int {
+	min := math.MaxInt
+	for _, number := range numbers {
+		if number < min {
+			min = number
+		}
+	}
+	return min
+}
--- day_5/line.go
+++ day_5/line.go
@@ -1,5 +1,7 @@
 package day_5
 
+import "github.com/sebastiangeiger/advent-of-code-2021/common"
+
 type Line struct {
 	start Point
 	end   Point
@@ -14,9 +16,25 @@ func (line *Line) IsVertical() bool {
 }
 
 func (line *Line) ToPoints() []Point {
-	// This is not right yet
+	if !line.IsHorizontal() && !line.IsVertical() {
+		panic("Only works on horizontal/vertical lines!")
+	}
 	result := []Point{}
-	result = append(result, line.start)
-	result = append(result, line.end)
+	if line.IsHorizontal() {
+		xStart := common.Min(line.start.x, line.end.x)
+		xEnd := common.Max(line.start.x, line.end.x)
+		y := line.start.y
+		for x := xStart; x <= xEnd; x++ {
+			result = append(result, Point{x, y})
+		}
+	}
+	if line.IsVertical() {
+		yStart := common.Min(line.start.y, line.end.y)
+		yEnd := common.Max(line.start.y, line.end.y)
+		x := line.start.x
+		for y := yStart; y <= yEnd; y++ {
+			result = append(result, Point{x, y})
+		}
+	}
 	return result
 }
