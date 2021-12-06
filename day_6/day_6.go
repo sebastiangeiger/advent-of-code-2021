1c2436da617735fe14da30d79a0ef2ab39a20a5a
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-06 09:06:23 -0500

    Printing 18 days

--- day_6/day_6.go
+++ day_6/day_6.go
@@ -2,6 +2,8 @@ package day_6
 
 import (
 	"fmt"
+	"strconv"
+	"strings"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -27,9 +29,26 @@ func problem2() {
 
 func solveProblem1(path string, printDebug bool) int {
 	line := common.ReadLinesFrom(path, false)[0]
-	initialPop := common.ToIntLine(line, ",")
-	if printDebug {
-		fmt.Printf("%#v\n", initialPop)
+	population := common.ToIntLine(line, ",")
+	maxDays := 18
+	for day := 0; day <= maxDays; day++ {
+		if printDebug {
+			dayDisplay(day, population)
+		}
+		population = simulateDay(population)
 	}
 	return 1
+}
+
+func simulateDay(population []int) []int {
+	return population
+}
+
+func dayDisplay(day int, population []int) {
+	result := []string{}
+	for _, pop := range population {
+		result = append(result, strconv.Itoa(pop))
+	}
+	strResult := strings.Join(result, ",")
+	fmt.Printf("After %2d days: %s\n", day, strResult)
 }
