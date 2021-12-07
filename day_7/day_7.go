921777224c318b9d518de325b97dc3c6f9af7dee
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-07 08:26:20 -0500

    Able to solve test for problem 1

--- day_7/day_7.go
+++ day_7/day_7.go
@@ -2,6 +2,7 @@ package day_7
 
 import (
 	"fmt"
+	"math"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -18,9 +19,28 @@ func Run(problem int) {
 }
 
 func problem1() {
-	fmt.Println("Day 7 - Problem 1")
+	solution := solveProblem1("day_7_test.input")
+	fmt.Printf("Minimum Fuel Cost (test): %d", solution)
 }
 
 func problem2() {
 	fmt.Println("Day 7 - Problem 2")
+}
+
+func solveProblem1(path string) int {
+	line := common.ReadLinesFrom(path, false)[0]
+	positions := common.ToIntLine(line, ",")
+	fuelCosts := []int{}
+	for i := common.Min(positions...); i <= common.Max(positions...); i++ {
+		fuelCosts = append(fuelCosts, alignTo(positions, i))
+	}
+	return common.Min(fuelCosts...)
+}
+
+func alignTo(positions []int, alignment int) int {
+	cost := 0.0
+	for _, position := range positions {
+		cost += math.Abs(float64(position) - float64(alignment))
+	}
+	return int(cost)
 }
