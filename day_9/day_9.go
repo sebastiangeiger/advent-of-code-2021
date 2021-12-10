3bc98a36f2a84e3e43055e4191b3d79ac370e2b0
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-09 23:40:19 -0500

    Order methods

--- day_9/day_9.go
+++ day_9/day_9.go
@@ -58,6 +58,23 @@ func problem2() {
 	fmt.Printf("Basins (test): %d\n", solveProblem2("day_9_test.input"))
 }
 
+func solveProblem1(path string) int {
+	matrix := readMatrix(path)
+	sum := 0
+	for _, p := range findLowPoints(matrix) {
+		sum += (matrix[p.x][p.y] + 1)
+	}
+
+	return sum
+}
+
+func solveProblem2(path string) int {
+	matrix := readMatrix(path)
+	basins := initializeBasins(matrix)
+	fmt.Println(basins)
+	return 1
+}
+
 func findLowPoints(matrix [][]int) []Point {
 	dx := len(matrix)
 	dy := len(matrix[0])
@@ -78,23 +95,6 @@ func findLowPoints(matrix [][]int) []Point {
 		}
 	}
 	return lowPoints
-}
-
-func solveProblem1(path string) int {
-	matrix := readMatrix(path)
-	sum := 0
-	for _, p := range findLowPoints(matrix) {
-		sum += (matrix[p.x][p.y] + 1)
-	}
-
-	return sum
-}
-
-func solveProblem2(path string) int {
-	matrix := readMatrix(path)
-	basins := initializeBasins(matrix)
-	fmt.Println(basins)
-	return 1
 }
 
 func initializeBasins(matrix [][]int) [][]Point {
