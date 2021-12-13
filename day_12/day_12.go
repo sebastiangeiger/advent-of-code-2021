d29a84266e93c07f4a027d5d135103e878624ff6
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-13 13:32:46 +0000

    Read everything into a data model

--- day_12/day_12.go
+++ day_12/day_12.go
@@ -2,6 +2,7 @@ package day_12
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -17,10 +18,34 @@ func Run(problem int) {
 	}
 }
 
+type Node struct {
+	name string
+}
+
+type Edge struct {
+	start Node
+	end   Node
+}
+
 func problem1() {
-	fmt.Println("Day 12 - Problem 1")
+	fmt.Printf("Number of paths (test): %d\n", solveProblem1("day_12_test.input"))
 }
 
 func problem2() {
 	fmt.Println("Day 12 - Problem 1")
+}
+
+func solveProblem1(path string) int {
+	edges := read(path)
+	fmt.Printf("%#v\n", edges)
+	return 1
+}
+
+func read(path string) []Edge {
+	edges := []Edge{}
+	for _, line := range common.ReadLinesFrom(path, false) {
+		points := strings.Split(line, "-")
+		edges = append(edges, Edge{Node{points[0]}, Node{points[1]}})
+	}
+	return edges
 }
