1a86da6367dbe7f5004d3d27bddfc6c1d3f1538f
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-13 14:43:09 +0000

    First attempt

--- day_12/day_12.go
+++ day_12/day_12.go
@@ -22,6 +22,11 @@ type Node struct {
 	name string
 }
 
+func (node Node) CanBeRevisited() bool {
+	firstLetter := node.name[0:1]
+	return firstLetter == strings.ToUpper(firstLetter)
+}
+
 type Edge struct {
 	start Node
 	end   Node
@@ -40,12 +45,22 @@ func solveProblem1(path string) int {
 	neighbors := makeNeighbors(edges)
 	printNeighbors(neighbors)
 	paths := [][]Node{[]Node{Node{"start"}}}
+	fmt.Println("Initial")
 	printPaths(paths)
-	return 1
+	i := 0
+	for {
+		i++
+		paths = expand(paths, neighbors)
+		fmt.Printf("Expansion %d\n", i+1)
+		printPaths(paths)
+		if allDone(paths) {
+			break
+		}
+	}
+	return len(paths)
 }
 
 func printPaths(paths [][]Node) {
-	fmt.Printf("%d paths\n", len(paths))
 	for i, path := range paths {
 		fmt.Printf("%3d: ", i)
 		nodeNames := make([]string, len(path))
@@ -64,6 +79,54 @@ func printNeighbors(neighbors map[Node][]Node) {
 		}
 		fmt.Printf("%s -> %s\n", key.name, strings.Join(names, ","))
 	}
+}
+
+func allDone(paths [][]Node) bool {
+	done := true
+	for _, path := range paths {
+		last := path[len(path)-1]
+		if last.name != "end" {
+			done = false
+			break
+		}
+	}
+	return done
+}
+
+func expand(paths [][]Node, neighbors map[Node][]Node) [][]Node {
+	result := [][]Node{}
+	for _, path := range paths {
+		last := path[len(path)-1]
+		if last.name == "end" {
+			result = append(result, path)
+		} else {
+			for _, neighbor := range neighbors[last] {
+				if neighbor.CanBeRevisited() {
+					newPath := make([]Node, len(path)+1)
+					copy(newPath, path)
+					newPath[len(path)] = neighbor
+					result = append(result, newPath)
+				} else if !includes(path, neighbor) {
+					newPath := make([]Node, len(path)+1)
+					copy(newPath, path)
+					newPath[len(path)] = neighbor
+					result = append(result, newPath)
+				}
+			}
+		}
+	}
+	return result
+}
+
+func includes(path []Node, node Node) bool {
+	found := false
+	for _, n := range path {
+		if n.name == node.name {
+			found = true
+			break
+		}
+	}
+	return found
 }
 
 func makeNeighbors(edges []Edge) map[Node][]Node {
