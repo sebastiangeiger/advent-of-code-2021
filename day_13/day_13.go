11df9e47ad10eae917b68c9a8039420a526ceff8
Sebastian Geiger <sebastian.geiger@gmail.com>
2021-12-13 21:29:03 -0500

    Able to read the problem

--- day_13/day_13.go
+++ day_13/day_13.go
@@ -2,6 +2,8 @@ package day_13
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 
 	"github.com/sebastiangeiger/advent-of-code-2021/common"
 )
@@ -17,10 +19,46 @@ func Run(problem int) {
 	}
 }
 
+type FoldInstruction struct {
+	axis       string
+	coordinate int
+}
+
 func problem1() {
-	fmt.Println("Day 13 - Problem 1")
+	solveProblem1("day_13_test.input")
+}
+
+func solveProblem1(path string) {
+	paper, foldInstructions := read(path)
+	fmt.Printf("Paper: %#v\n", paper)
+	fmt.Printf("FoldInstructions: %#v\n", foldInstructions)
 }
 
 func problem2() {
 	fmt.Println("Day 13 - Problem 2")
+}
+
+func read(path string) ([][]int, []FoldInstruction) {
+	lines := common.ReadLinesFrom(path, true)
+	coordinates := [][]int{}
+	foldInstructions := []FoldInstruction{}
+	readCoordinates := true
+	reg := regexp.MustCompile("^fold along (x|y)=(\\d+)$")
+	for _, line := range lines {
+		if readCoordinates && line != "" {
+			coordinates = append(coordinates, common.ToIntLine(line, ","))
+		} else if readCoordinates && line == "" {
+			readCoordinates = false
+		} else if !readCoordinates && line != "" {
+			matches := reg.FindStringSubmatch(line)
+			axis := matches[1]
+			coordinate, err := strconv.Atoi(matches[2])
+			if err == nil {
+				foldInstructions = append(foldInstructions, FoldInstruction{axis, coordinate})
+			} else {
+				panic("Down here!")
+			}
+		}
+	}
+	return coordinates, foldInstructions
 }
